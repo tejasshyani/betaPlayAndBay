329c43dcb2d8210ad0b591f29e44ded24f0f396f
prachi <prachiwaliaodesk@gmail.com>
2013-12-16 23:28:21 +0530

    navigation

--- oc-content/themes/bender/item.php
+++ oc-content/themes/bender/item.php
@@ -146,7 +146,7 @@
                             <input type="hidden" name="action" value="add_comment" />
                             <input type="hidden" name="page" value="item" />
                             <input type="hidden" name="id" value="<?php echo osc_item_id(); ?>" />
-                           <!-- <?php if(osc_is_web_user_logged_in()) { ?>
+                           <?php if(osc_is_web_user_logged_in()) { ?>
                                 <input type="hidden" name="authorName" value="<?php echo osc_esc_html( osc_logged_user_name() ); ?>" />
                                 <input type="hidden" name="authorEmail" value="<?php echo osc_logged_user_email();?>" />
                             <?php } else { ?>
@@ -155,8 +155,8 @@
                                    
                                         <?php CommentForm::email_input_text(); ?>
                                    
-                            <?php }; ?>-->
-                              
+                            <?php }; ?>
+                              <input type="hidden" name="authorEmail" value="<?php echo osc_logged_user_email();?>" />
                                     <?php CommentForm::body_input_textarea(); ?>
                                 
                             <div class="clear"></div>
