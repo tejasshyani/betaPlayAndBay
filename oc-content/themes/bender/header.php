054da952ea058768fe5f2cffc4f983f01408fcb4
prachi <prachiwaliaodesk@gmail.com>
2013-12-16 23:50:48 +0530

    comment

--- oc-content/themes/bender/header.php
+++ oc-content/themes/bender/header.php
@@ -52,7 +52,7 @@ jQuery(document).ready(function(){
 	        return this; // for chaining...
        }
 	   
-	jQuery("nav.navigation ul.nav > li > a [class!='no-link']").click(function(e){
+	jQuery("nav.navigation ul.nav > li > a").click(function(e){
 		e.preventDefault();
 		var $this = jQuery(this);
 		 jQuery("nav.navigation ul.nav > li").removeClass("current");
